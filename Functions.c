e16031520275dab0ebad6c5c63fb10a920910a4d
AmolMane30 <maneamol3001@gmail.com>
2024-08-22 22:07:33 +0530

    Add files via upload

---
+++ Functions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Display()
+{
+    printf("Hello I am Amol\n");
+}
+int main()
+{
+    printf("Inside main function\n");
+
+    Display();
+
+    printf("End of main\n");
+
+    return 0;
+}
